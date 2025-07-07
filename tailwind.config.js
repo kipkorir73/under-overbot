f48a441729d68d728f66c14761019d376ae717b1
kipkorir73 <123023142+kipkorir73@users.noreply.github.com>
2025-07-07 18:21:27 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-850': 'rgb(31, 41, 55)',
+      }
+    },
+  },
+  plugins: [],
+}
